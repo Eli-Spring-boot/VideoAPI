bf3dfda5be7e9ebafe18abefea1b2d9f38280b17
Eli <elikembuertey@gmail.com>
2023-07-01 15:36:37 -0400

    removed unnecessary folders

--- demofake/src/main/java/com/example/demofake/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demofake;
-
-import org.springframework.data.repository.Repository;
-import com.example.demofake.UserAccount;
-
-public interface UserRepository extends Repository<UserAccount, Long>{
-	UserAccount findByUsername(String username);
-}
