8a1db71c8ba7b9f6d209fc2e93fb2582a77001d7
Eli <elikembuertey@gmail.com>
2023-07-01 16:40:13 -0400

    fixed a bug with the converter from application.properties string to springboot role

--- demo/src/main/java/com/example/demo/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -15,6 +19,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+	interface GrantedAuthorityCnv extends Converter<String, GrantedAuthority> {}
+
+	@Bean
+	@ConfigurationPropertiesBinding
+	GrantedAuthorityCnv converter() {
+		return SimpleGrantedAuthority::new;
+	}
+
 	@Bean
 	UserDetailsService userService(UserRepository repo) {
 		return username -> repo.findByUsername(username).asUser();
